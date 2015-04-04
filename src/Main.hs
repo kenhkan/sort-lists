0bec9b5e9464fcf5886c323c9e7b3e612f69e430
Kenneth Kan <ken@pixbi.com>
2015-04-03 23:08:15 -0400

    Implement

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,13 @@
 module Main where
 
 import           System.Environment              (getArgs)
+import Data.List (sortBy)
+
+-- Manually set the expected input type, which is irrelevant in this
+-- challenge anyway.
+type InputType = Int
+
+type List a = [a]
 
 data Order = Asc | Desc
 
@@ -17,12 +24,18 @@ main = do
     let order = read (if (null args) then "asc" else head args) :: Order
 
     line <- getLine
-    -- Manually set the expected type, which is irrelevant in this
-    -- challenge anyway.
-    let lists = read line :: [[Int]]
+    let lists = read line :: [[InputType]]
 
-    let display = putStrLn . show . concat
+    let display = putStrLn . show
     display . sortLists order $ lists
 
-sortLists :: Order -> [[a]] -> [[a]]
-sortLists _ lists = lists
+sortLists :: Order -> [List a] -> [List a]
+sortLists order lists = sortBy (compareLists order) lists
+
+-- This creates a compare predicate for sorting.
+compareLists :: Order -> (List a -> List a -> Ordering)
+compareLists Asc a b  = if compareLists' a b then LT else GT
+compareLists Desc a b = if compareLists' a b then GT else LT
+
+compareLists' :: List a -> List a -> Bool
+compareLists' a b = length a < length b
